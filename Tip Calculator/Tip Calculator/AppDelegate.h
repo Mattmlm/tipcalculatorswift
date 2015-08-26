1d374c8c2163df0decf4b643bcbcaf1cc7fc7128
Matt Mo <matthew.l.mo@gmail.com>
2015-08-26 13:57:49 -0700

    Create xcode project

---
+++ Tip Calculator/Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by admin on 8/26/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by admin on 8/26/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
