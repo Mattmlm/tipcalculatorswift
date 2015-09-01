89ad997ea3f193e3088b33e4b4bc13ac55201294
Matt Mo <matthew.l.mo@gmail.com>
2015-09-01 01:48:14 -0700

    [New files] - add constants and settings

---
+++ Tip Calculator/Tip Calculator/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Tip Calculator
+//
+//  Created by admin on 8/31/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+/*
+*Colors
+*/
---
+++ Tip Calculator/Tip Calculator/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Tip Calculator
+//
+//  Created by admin on 9/1/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+    }
+}
