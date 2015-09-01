cd34c74e6f25e7c103f5994e6156f976debbbea1
Matt Mo <matthew.l.mo@gmail.com>
2015-09-01 14:22:45 -0700

    [Settings] - enable user to select currency

--- Tip Calculator/Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/Tip Calculator/SettingsViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var currencyPickerView: UIPickerView!;
+    @IBOutlet weak var currencyDisplayLabel: UILabel!;
+    @IBOutlet weak var currencyPickerToolbar: UIToolbar!;
+    var currencyPickerData: Array<String> = [];
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        var countriesArray = getAllCountryLocales();
-        for countryCode in countriesArray {
-            println("locale is \(countryCode)");
-        }
+        self.setUpCurrencyPicker();
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -26,17 +28,45 @@ class SettingsViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor();
     }
     
-    func getAllCountryLocales() -> Array<String> {
-        let locale = NSLocale.currentLocale()
-        let countryArray = NSLocale.ISOCountryCodes()
-        var unsortedCountryArray:[String] = []
-        for countryCode in countryArray {
-            let displayNameString = locale.displayNameForKey(NSLocaleCountryCode, value: countryCode)
-            if displayNameString != nil {
-                unsortedCountryArray.append(displayNameString!)
-            }
-        }
-        let sortedCountryArray = sorted(unsortedCountryArray, <)
-        return sortedCountryArray;
+    func setUpCurrencyPicker() {
+        
+        // Set delegate and datasource
+        currencyPickerView.delegate = self;
+        currencyPickerView.dataSource = self;
+        
+        // Setup UX interactions to select currency setting
+        var selectCurrencyDisplayLabel: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "currencyLabelSelected:");
+        self.currencyDisplayLabel.addGestureRecognizer(selectCurrencyDisplayLabel);
+        
+        // Setup Currency options data
+        let unsortedCountryNamesArray:[String] = NSLocale.commonISOCurrencyCodes() as! [String];
+        let sortedCountryArray = sorted(unsortedCountryNamesArray, <);
+        self.currencyPickerData = sortedCountryArray;
+    }
+    
+    @IBAction func finishSelectingCurrency(sender:UIBarButtonItem) {
+        self.currencyPickerView.hidden = true;
+        self.currencyPickerToolbar.hidden = true;
+        currencyDisplayLabel.text = currencyPickerData[currencyPickerView.selectedRowInComponent(0)];
+    }
+    
+    @IBAction func currencyLabelSelected(sender: UILabel) {
+        self.currencyPickerView.hidden = false;
+        self.currencyPickerToolbar.hidden = false;
+    }
+    
+    //MARK: UIPickerViewDataSource
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    // returns the # of rows in each component..
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.currencyPickerData.count;
+    }
+    
+    //MARK: UIPickerViewDelegate
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return currencyPickerData[row];
     }
 }
