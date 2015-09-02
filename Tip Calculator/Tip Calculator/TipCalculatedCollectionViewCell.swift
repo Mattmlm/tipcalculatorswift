7d69afc3d254c75f43405ae4ee02b54ef2560b04
Matt Mo <matthew.l.mo@gmail.com>
2015-09-01 17:26:23 -0700

    [Calculated Tip View] - import nib from previous version

---
+++ Tip Calculator/Tip Calculator/TipCalculatedCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TipCalculatorCollectionViewCell.swift
+//  Tip Calculator
+//
+//  Created by admin on 9/1/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+class TipCalculatedCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var totalCalculatedValueLabel: UILabel!
+    @IBOutlet weak var tipCalculatedValueLabel: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    var tipPercentage: NSNumber = 0.0;
+    
+    class func nibNameForCell() -> String {
+        return "TipCalculatedCollectionViewCell";
+    }
+    
+    class func reuseIdentiferForCell() -> String {
+        return "tipCell";
+    }
+    
+    func updateCell(billTotal: Double, numberOfPeople: Int) {
+        let tipCalculated: Double = self.tipPercentage.doubleValue * 0.01 * billTotal / Double(numberOfPeople);
+        let totalCalculated: Double = (self.tipPercentage.doubleValue * 0.01 + 1) * billTotal / Double(numberOfPeople);
+        self.tipPercentageLabel.text = self.tipPercentage.stringValue + "%";
+        self.tipCalculatedValueLabel.text = CurrencyFormatter.sharedInstance.stringFromNumber(tipCalculated)! + ",";
+        self.totalCalculatedValueLabel.text = CurrencyFormatter.sharedInstance.stringFromNumber(totalCalculated);
+    }
+}
--- Tip Calculator/Tip Calculator/TipCalculatorViewController.swift
+++ Tip Calculator/Tip Calculator/TipCalculatorViewController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
-class TipCalculatorViewController: UIViewController, CurrencyTextFieldDelegate {
+class TipCalculatorViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CurrencyTextFieldDelegate {
 
     @IBOutlet weak var billTotalField: CurrencyTextField!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var tipCalculatedCollectionView: UICollectionView!
+    @IBOutlet weak var numberOfPeopleView: UIView!
+    @IBOutlet weak var numberOfPeopleLabel: UILabel!
+    
     var currencyTextFieldDelegate: CurrencyTextFieldController = CurrencyTextFieldController();
     
     override func viewDidLoad() {
@@ -18,13 +23,27 @@ class TipCalculatorViewController: UIViewController, CurrencyTextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         
         billTotalField.delegate = currencyTextFieldDelegate;
+        currencyTextFieldDelegate.formatter = CurrencyFormatter.sharedInstance;
         currencyTextFieldDelegate.delegate = self;
         self.billTotalField.becomeFirstResponder();
+
+        var tipCellNib = UINib(nibName: TipCalculatedCollectionViewCell.nibNameForCell(), bundle: nil);
+        self.tipCalculatedCollectionView.registerNib(tipCellNib, forCellWithReuseIdentifier: TipCalculatedCollectionViewCell.reuseIdentiferForCell());
+        self.tipCalculatedCollectionView.delegate = self;
+        self.tipCalculatedCollectionView.dataSource = self;
+        
+        self.loadLatestData();
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated);
         self.navigationController?.setNavigationBarHidden(true, animated: true);
+        self.loadDefaultData();
+        self.clearLatestData();
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews();
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +51,46 @@ class TipCalculatorViewController: UIViewController, CurrencyTextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Helpers
+    func loadLatestData() {
+        
+    }
+    
+    func clearLatestData() {
+        
+    }
+    
+    func loadDefaultData() {
+        
+    }
+    
+    //MARK: UICollectionViewDataSource
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(
+            TipCalculatedCollectionViewCell.reuseIdentiferForCell(), forIndexPath: indexPath) as! TipCalculatedCollectionViewCell;
+        cell.tipPercentage = indexPath.row;
+        let billTotalNumber = CurrencyFormatter.sharedInstance.numberFromString(self.billTotalField.text);
+        let billTotalDouble = billTotalNumber!.doubleValue;
+        cell.updateCell(billTotalDouble, numberOfPeople: 1);
+
+        return cell;
+    }
+    
+    //MARK: UICollectionViewDelegateFlowLayout protocol methods
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(UIScreen.mainScreen().bounds.size.width / 2.0, self.tipCalculatedCollectionView.frame.size.height);
+    }
+    
+    //MARK: CurrencyTextFieldDelegate
     func currencyTextField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String, fieldValue: Float) -> Bool {   
         return true;
     }
