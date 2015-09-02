65d58121f02e03ac32a7942231fe3c843ecdc10f
Matt Mo <matthew.l.mo@gmail.com>
2015-09-01 17:25:44 -0700

    [Utils] - add numberformatter singleton

---
+++ Tip Calculator/Tip Calculator/CurrencyFormatter.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyFormatter.swift
+//  Tip Calculator
+//
+//  Created by admin on 9/1/15.
+//  Copyright (c) 2015 mattmo. All rights reserved.
+//
+
+import UIKit
+
+public class CurrencyFormatter: NSNumberFormatter {
+    required public init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init() {
+        super.init()
+        self.locale = NSLocale.currentLocale()
+        self.maximumFractionDigits = 2
+        self.minimumFractionDigits = 2
+        self.alwaysShowsDecimalSeparator = true
+        self.numberStyle = .CurrencyStyle
+    }
+    
+    // Swift 1.2 or above
+    //  static let sharedInstance = CurrencyFormatter()
+    class var sharedInstance: CurrencyFormatter {
+        struct Static {
+            static let instance = CurrencyFormatter()
+        }
+        return Static.instance
+    }
+}
